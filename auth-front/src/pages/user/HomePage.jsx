1badfa8c12db7bd7d4f43f5947e7fa923686b39c
batchlcwd <batchlcwd@gmail.com>
2025-11-08 19:09:31 +0530

    completed auth app, with email and password login, login with google and login with github

---
+++ auth-front/src/pages/user/HomePage.jsx
@@ -0,0 +1,14 @@
+import { useAuthStore } from "@/components/auth/auth";
+import React from "react";
+import DashboardCmp from "./DashboardCmp";
+
+function HomePage() {
+  const user = useAuthStore((state) => state.user);
+  return (
+    <div>
+      <DashboardCmp />
+    </div>
+  );
+}
+
+export default HomePage;
